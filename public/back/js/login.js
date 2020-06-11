f0ddbdfee3ad3053da52275c9aa0397852440a26
sky2121 <599140251@qq.com>
2020-06-11 12:37:17 +0800

    ajax登录校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -7,6 +7,14 @@ $(function () {
 *
 * */
 $("#form").bootstrapValidator({
+
+    // 配置校验图标
+    feedbackIcons: {
+        valid: 'glyphicon glyphicon-ok',    // 校验成功
+        invalid: 'glyphicon glyphicon-remove',  // 校验失败
+        validating: 'glyphicon glyphicon-refresh' // 校验中
+    },
+
     fields:{
         username:{
         //    配置校验规则
@@ -20,7 +28,11 @@ $("#form").bootstrapValidator({
                     min:2,
                     max:6,
                     message:"密码长度必须是2-6位"
-                }
+                },
+            //  专门用于配置回调提示的规则
+            callback:{
+               message:"用户名不存在"
+            }
             }
 
         },
@@ -35,10 +47,64 @@ $("#form").bootstrapValidator({
                     min:6,
                     max:12,
                     message:"密码长度必须是6-12位"
+                },
+                callback:{
+                    message:"密码错误"
                 }
             }
         }
     }
-})
+});
+
+    /*
+    * 2.登录功能
+    *   表单校验插件会在提交表单时进行校验
+    *   （1）校验成功：默认提交表单，会发生页面跳转，
+    *       我们需要注册表单校验成功事件，阻止默认的提交，通过ajax进行发送请求
+    *   （2）校验失败，不会提交表单，配置插件提示用户即可
+    *
+    * */
+
+//    注册表单校验成功事件
+    $("#form").on("success.form.bv",function (e) {
+    //    阻止默认的表单提交
+        e.preventDefault();
+
+    //    通过ajax进行提交
+        $.ajax({
+            type:"post",
+            url:"/employee/employeelogin",
+            data:$("#form").serialize(),
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                if(info.success){
+                    location.href="index.html"
+                }
+                if(info.error === 1000){
+                    //INVALID 失败 VALID  NOT_VALIDATED   VALIDATED
+                    $("form").data("bootstrapValidator").updateStatus("username","INVALID","callback")
+                }
+                if(info.error === 1001){
+                    $("form").data("bootstrapValidator").updateStatus("password","INVALID","callback")
+                }
+
+            }
+        })
+
+    });
+
+
+    /*
+   * 3.重置功能
+   *
+   * */
+    $('[type="reset"]').click(function () {
+    //    调用插件的方法，进行重置校验状态
+    //     resetForm(boolean)
+    //    true:重置内容以及校验状态
+    //    false:只重置校验状态
+        $("#form").data("bootstrapValidator").resetForm(true);
+    });
 
 })
