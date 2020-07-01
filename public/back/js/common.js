48c1445c0f338bb094c57caccc8d5d52e42f5419
sky2121 <599140251@qq.com>
2020-07-01 12:46:50 +0800

    首页完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -5,3 +5,62 @@ $(document).ajaxStart(function () {
 $(document).ajaxStop(function () {
     NProgress.done();
 })
+
+//登录拦截功能
+if(location.href.indexOf("login.html") === -1){
+$.ajax({
+    url:"/employee/checkRootLogin",
+    type:"GET",
+    datatype:"json",
+    success:function (info) {
+        if(info.success){
+        //    说明已经登录
+        }
+        if(info.error === 400){
+        //    未登录，拦截到登录也
+            location.href = "login.html"
+        }
+
+    }
+})
+}
+
+
+//入口函数里面写
+$(function(){
+//1.    分类管理的切换功能
+    $(".nav .category").click(function () {
+        $(".nav .child").stop().slideToggle();
+    })
+//    2.
+    $(".icon_menu").click(function () {
+        $(".lt_aside").addClass("hidemenu");
+        $(".lt_topbar").addClass("hidemenu");
+        $(".it_main").addClass("hidemenu");
+    })
+//    3.点击topbar按钮退出模态框
+    $(".icon_out").click(function () {
+        $("#logoutModal").modal("show")
+
+    })
+//    4.点击模态框的退出按钮，实现退出功能
+    $("#logoutBtn").click(function () {
+    //    发送ajax请求，进行退出
+        $.ajax({
+            url:"/employee/employeeLogout",
+            type:"GET",
+            dataType:"json",
+            success:function (info) {
+                console.log(info);
+                if (info.success){
+                    location.href="login.html"
+                }
+
+            }
+
+        })
+
+    })
+
+
+})
---
+++ public/back/js/index.js
@@ -0,0 +1,81 @@
+$(function () {
+// 基于准备好的dom，初始化echarts实例
+    var echarts_1 = echarts.init(document.querySelector('.echarts_1'));
+
+// 指定图表的配置项和数据
+    var option1 = {
+        title: {
+            text: 'ECharts 入门示例'
+        },
+        //提示框组件
+        tooltip: {},
+        //图例
+        legend: {
+            data:['人数']
+        },
+        //x轴刻度
+        xAxis: {
+            data: ["1月","2月","3月","4月","5月","6月"]
+        },
+        //y轴的刻度，注意y轴的刻度应该根据数据动态生成
+        yAxis: {},
+        series: [{
+            name: '人数',
+            type: 'bar',
+            data: [1500, 2000, 1800, 1200, 1400, 2500]
+        }]
+    };
+
+// 使用刚指定的配置项和数据显示图表。
+    echarts_1.setOption(option1);
+
+
+//    饼图
+    // 基于准备好的dom，初始化echarts实例
+    var echarts_2 = echarts.init(document.querySelector('.echarts_2'));
+
+    option2 = {
+        //大标题
+        title: {
+            text: '热门品牌销售',
+            subtext: '2017年6月',
+            x: 'right'
+        },
+        //提示框组件
+        tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+        },
+        legend: {
+            orient: 'vertical',
+            left: 'left',
+            data: ['耐克', '阿迪', '新百伦', '李宁', '阿迪王']
+        },
+        series: [
+            {
+                name: '品牌',
+                type: 'pie',
+                radius: '55%',
+                center: ['50%', '60%'],
+                data: [
+                    {value: 335, name: '耐克'},
+                    {value: 310, name: '阿迪'},
+                    {value: 234, name: '新百伦'},
+                    {value: 135, name: '李宁'},
+                    {value: 1548, name: '阿迪王'}
+                ],
+                //添加阴影效果
+                emphasis: {
+                    itemStyle: {
+                        shadowBlur: 10,
+                        shadowOffsetX: 0,
+                        shadowColor: 'rgba(0, 0, 0, 0.5)'
+                    }
+                }
+            }
+        ]
+    };
+// 使用刚指定的配置项和数据显示图表。
+    echarts_2.setOption(option2);
+
+})
